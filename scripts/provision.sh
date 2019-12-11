07252eac081c44a91e331ed2833a28bf5b0b0221
Gauthier Delamarre <gdelamarre@gmail.com>
2019-12-11 14:51:06 +0100

    Update provisioner

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -34,26 +34,25 @@ mv /tmp/apps /home/deckle
 echo "Done"
 echo ""
 
-echo "Enforcing files ownership to deckle user..."
-sudo chown -R deckle "/home/deckle"
-echo "Done"
-echo ""
-
 # prepare projects directory
 if [ ! -d "/home/deckle/projects" ]; then
   echo "Creating projects target directory"
   mkdir /home/deckle/projects
+  echo ""
 fi
 
 
 # Configuring Docker
 if [ -d "/tmp/docker.service.d" ]; then
+  if [ -d "/etc/systemd/system/docker.service.d" ]; then
+    rm -rf "/etc/systemd/system/docker.service.d";
+  fi
 mv /tmp/docker.service.d /etc/systemd/system/
 fi
 
-if [ ! "$(docker network ls | grep deckle)" ]; then
-echo "Creating the deckle docker network"
-docker network create deckle
+if ! docker network ls | grep -q deckle; then
+  echo "Creating the deckle docker network"
+  docker network create deckle
 else
   echo "Docker network deckle already exists"
 fi
@@ -65,16 +64,26 @@ usermod -G docker vagrant
 # starting docker apps
 echo "Starting deckle apps..."
 if [ -d "/home/deckle/apps" ]; then
-cd /home/deckle/apps
-for app in `ls . `; do
-  cd $app;
-  docker-compose up -d
-  cd ..;
-done;
+  cd /home/deckle/apps || exit;
+  while IFS= read -d $'\0' -r dir ; do
+    (
+      cd "$dir" || exit
+      if [ -f docker-compose.yml ]; then
+        docker-compose up -d
+      fi
+    )
+  done < <(find . -mindepth 1 -maxdepth 1 -type d -print0)
 else
   echo "Unable to access the apps directory (/home/deckle/apps)"
 fi
 
 # install oh my zsh
-echo "Installing Oh My Zsh..."
-runuser -l deckle -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
+if [ ! -d /home/deckle/.oh-my-zsh ]; then
+  echo "Installing Oh My Zsh..."
+  runuser -l deckle -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
+fi
+
+echo "Enforcing files ownership to deckle user..."
+sudo chown -R deckle "/home/deckle"
+echo "Done"
+echo ""
